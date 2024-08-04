4c74042b2d4242728a1f390fc061b025a9c7ce48
Manthan <manthan2318@gmail.com>
2024-08-04 17:48:56 +0530

    Error handling done

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -20,20 +21,29 @@ public class NumberGuessingGame {
 
             while (numberOfAttempts < maxAttempts) {
                 System.out.print("Enter your guess: ");
-                int userGuess = scanner.nextInt();
-                numberOfAttempts++;
+                try {
+                    int userGuess = scanner.nextInt();
+                    if (userGuess < 1 || userGuess > 100) {
+                        System.out.println("Please enter a number between 1 and 100.");
+                        continue;
+                    }
+                    numberOfAttempts++;
 
-                if (userGuess < numberToGuess) {
-                    System.out.println("Too low!");
-                } else if (userGuess > numberToGuess) {
-                    System.out.println("Too high!");
-                } else {
-                    System.out.println("Congratulations! You guessed the correct number.");
-                    hasWon = true;
-                    break;
+                    if (userGuess < numberToGuess) {
+                        System.out.println("Too low!");
+                    } else if (userGuess > numberToGuess) {
+                        System.out.println("Too high!");
+                    } else {
+                        System.out.println("Congratulations! You guessed the correct number.");
+                        hasWon = true;
+                        break;
+                    }
+
+                    System.out.println("Attempts left: " + (maxAttempts - numberOfAttempts));
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    scanner.next(); // Clear the invalid input
                 }
-
-                System.out.println("Attempts left: " + (maxAttempts - numberOfAttempts));
             }
 
             if (!hasWon) {
@@ -41,7 +51,8 @@ public class NumberGuessingGame {
             }
 
             System.out.print("Do you want to play again? (yes/no): ");
-            String response = scanner.next();
+            scanner.nextLine(); // Consume the newline character
+            String response = scanner.nextLine();
             playAgain = response.equalsIgnoreCase("yes");
         }
 
